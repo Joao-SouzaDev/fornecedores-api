5c6cf4e91ab8d501fa9c51f75f0f9415a97e03a1
Joao-SouzaDev <joao.souzadev@outlook.com>
2024-11-10 21:46:40 -0300

    Adicionado exceções no cadastro de usuário

--- Models/DTOs/CreateUsuarioDto.cs
+++ Models/DTOs/CreateUsuarioDto.cs
@@ -5,7 +5,9 @@ namespace fornecedor_api.Models.DTOs;
 public class CreateUsuarioDto
 {
     [Required]
+    [MinLength(6, ErrorMessage = "O login deve ter mais que 6 caracteres")]
     public string Login { get; set; }
     [Required]
+    [MinLength(8, ErrorMessage = "A senha deve ter mais que 8 caracteres")]
     public string Senha { get; set; }
 }
